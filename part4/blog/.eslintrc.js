2b42d0e02667b5d41af3f8d49466c422df818fe1
Jani Pirisjoki <jpir@iki.fi>
2023-12-20 00:34:10 +0200

    EsLintrc

---
+++ part4/blog/.eslintrc.js
@@ -0,0 +1,42 @@
+// eslint config file
+
+module.exports = {
+    'env': {
+        'commonjs': true,
+        'es2021': true,
+        'node': true,
+	'jest': true
+    },
+    'extends': 'eslint:recommended',
+    'parserOptions': {
+        'ecmaVersion': 'latest'
+    },
+    'rules': {
+        'no-console': 0,
+        'eqeqeq': 'error',
+        'no-trailing-spaces': 'error',
+        'object-curly-spacing': [
+            'error',
+            'always'
+        ],
+        'arrow-spacing': [
+            'error', { 'before': true, 'after': true }
+        ],
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'off',
+            'windows'
+        ],
+        'quotes': [
+            'off',
+            'single'
+        ],
+        'semi': [
+            'off',
+            'never'
+        ],
+    }
+}
