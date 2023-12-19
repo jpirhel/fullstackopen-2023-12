52a5fdcefa5c0f1752464d7c12164182684452f1
Jani Pirisjoki <jpir@iki.fi>
2023-12-20 00:45:40 +0200

    Part4: Assignment 4.4

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -2,6 +2,25 @@ const dummy = (blogs) => {
     return 1;
 }
 
+const totalLikes = (blogs) => {
+    if (! Array.isArray(blogs)) {
+        return 0;
+    }
+
+    if (blogs.length < 1) {
+        return 0;
+    }
+
+    let likes = 0;
+
+    blogs.forEach(blog => {
+        likes += blog.likes;
+    });
+
+    return likes;
+}
+
 module.exports = {
     dummy,
+    totalLikes,
 }
