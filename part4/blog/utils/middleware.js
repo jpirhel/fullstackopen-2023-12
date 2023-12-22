54d8b8ee82314db8dd916a2a154727d5145ff09a
Jani Pirisjoki <jpir@iki.fi>
2023-12-22 02:22:43 +0200

    Fix potential crash

--- part4/blog/utils/middleware.js
+++ part4/blog/utils/middleware.js
@@ -1,22 +1,18 @@
 const _ = require("lodash");
 const tokenExtractor = (req, res, next) => {
-    const authorizationHeader = req.get("authorization");
+    const header = req.get("authorization");
 
     let addToken = true;
 
-    if (_.isEmpty(authorizationHeader)) {
+    if (_.isEmpty(header) || !header.startsWith(prefix)) {
         addToken = false;
     }
 
     const prefix = "Bearer ";
 
-    if (!authorizationHeader.startsWith(prefix)) {
-        addToken = false;
-    }
-
     if (addToken) {
         // noinspection UnnecessaryLocalVariableJS
-        const token = authorizationHeader.replace(prefix, "");
+        const token = header.replace(prefix, "");
 
         req.token = token;
     }
