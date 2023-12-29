840dd011f8ffc869f41f77753a762b06c5d2d3a3
Jani Pirisjoki <jpir@iki.fi>
2023-12-29 04:27:10 +0200

    Part5: Assignment 5.3

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -9,12 +9,24 @@ import loginService from "./services/login";
 const BlogList = (props) => {
     const blogs = _.get(props, "blogs") || [];
     const user = _.get(props, "user");
+
     const onSubmitLogout = _.get(props, "onSubmitLogout");
+    const onSubmitCreate = _.get(props, "onSubmitCreate");
+
+    const onChangeTitle = _.get(props, "onChangeTitle");
+    const onChangeAuthor = _.get(props, "onChangeAuthor");
+    const onChangeUrl = _.get(props, "onChangeUrl");
 
     return (
         <>
             {user.name} logged in
             <LogoutForm onSubmit={onSubmitLogout}/>
+            {user && <CreateForm
+                onSubmit={onSubmitCreate}
+                onChangeTitle={onChangeTitle}
+                onChangeAuthor={onChangeAuthor}
+                onChangeUrl={onChangeUrl}
+            />}
             <div>&nbsp;</div>
             {blogs.map(blog =>
                 <Blog key={blog.id} blog={blog}/>
@@ -63,18 +75,38 @@ const LogoutForm = ({onSubmit}) => {
     )
 }
 
+const CreateForm = ({onSubmit, onChangeTitle, onChangeAuthor, onChangeUrl}) => {
+    return (
+        <div>
+            <h2>create new</h2>
+            <form>
+                title: <input type="text" name="Title" onChange={({target}) => onChangeTitle(target.value)}/><br/>
+                author: <input type="text" name="Author" onChange={({target}) =>onChangeAuthor(target.value)}/><br/>
+                url: <input type="text" name="Url" onChange={({target}) => onChangeUrl(target.value)}/>
+            </form>
+            <button onClick={onSubmit}>create</button>
+        </div>
+    );
+}
 const App = () => {
     const [ready, setReady] = useState(false);
 
     const [blogs, setBlogs] = useState([])
+
     const [user, setUser] = useState(null);
     const [username, setUsername] = useState(null);
     const [password, setPassword] = useState(null);
 
+    const [title, setTitle] = useState("");
+    const [author, setAuthor] = useState("");
+    const [url, setUrl] = useState("");
+
+    const [refreshBlogs, setRefreshBlogs] = useState(0);
+
     // NOTE changed to depend on user, so blogs will be fetched after the user logs in
     useEffect(() => {
         getAllBlogs();
-    }, [user])
+    }, [user, refreshBlogs])
 
     useEffect(() => {
         const storedUserJSON = window.localStorage.getItem("user");
@@ -133,12 +165,37 @@ const App = () => {
         setPassword(data);
     };
 
+    const onChangeTitle = (data) => {
+        setTitle(data);
+    }
+
+    const onChangeAuthor = (data) => {
+        setAuthor(data);
+    }
+
+    const onChangeUrl = (data) => {
+        setUrl(data);
+    }
+    const onSubmitCreate = async (data) => {
+        const result = await blogService.postNew(user, title, author, url);
+
+        setRefreshBlogs(refreshBlogs + 1);
+    };
+
     const renderLoginForm = !user && ready;
 
     return (
         <div>
             {user && <h2>blogs</h2>}
-            {user && <BlogList blogs={blogs} user={user} onSubmitLogout={onSubmitLogout}/>}
+            {user && <BlogList
+                blogs={blogs}
+                user={user}
+                onSubmitLogout={onSubmitLogout}
+                onSubmitCreate={onSubmitCreate}
+                onChangeTitle={onChangeTitle}
+                onChangeAuthor={onChangeAuthor}
+                onChangeUrl={onChangeUrl}
+            />}
             {renderLoginForm &&
                 <LoginForm
                     username={username || ""}
--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -1,19 +1,40 @@
 import axios from 'axios'
 
-const url = (path) => {
-  // const baseUrl = "http://localhost:3003";
-  const baseUrl = "";
-  return `${baseUrl}${path}`;
+const serverUrl = (path) => {
+    // const baseUrl = "http://localhost:3003";
+    const baseUrl = "";
+    return `${baseUrl}${path}`;
 }
 
 const getAll = () => {
-  const getUrl = url("/api/blogs");
+    const getUrl = serverUrl("/api/blogs");
 
-  console.log("blogService.getAll getUrl:", getUrl);
+    console.log("blogService.getAll getUrl:", getUrl);
 
-  const request = axios.get(getUrl)
+    const request = axios.get(getUrl)
 
-  return request.then(response => response.data)
+    return request.then(response => response.data)
 }
 
-export default { getAll }
+const postNew = async (user, title, author, url) => {
+    const postUrl = serverUrl("/api/blogs");
+
+    const data = {user, title, author, url};
+
+    const headers = {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${user.token}`,
+    };
+
+    let result;
+
+    try {
+        result = await axios.post(postUrl, data, {headers});
+    } catch (e) {
+        return null;
+    }
+
+    return result;
+}
+
+export default {getAll, postNew};
