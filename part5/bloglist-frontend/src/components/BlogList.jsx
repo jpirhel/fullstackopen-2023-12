b3759da461fb9e69ec20b993bc801875a104a797
Jani Pirisjoki <jpir@iki.fi>
2023-12-31 03:30:52 +0200

    Part5: Assignment 5.10

--- part5/bloglist-frontend/src/components/BlogList.jsx
+++ part5/bloglist-frontend/src/components/BlogList.jsx
@@ -5,11 +5,11 @@ import Blog from "./Blog.jsx";
 const BlogList = (props) => {
     const blogs = _.get(props, "blogs") || [];
 
+    const sortedBlogs = _.reverse(_.sortBy(blogs, (blog) => blog.likes));
+
     return (
         <>
-            {blogs.map(blog =>
-                <Blog key={blog.id} blog={blog} user={props.user}/>
-            )}
+            {sortedBlogs.map(blog =><Blog key={blog.id} blog={blog} user={props.user}/>)}
         </>
     );
 }
